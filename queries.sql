1e0ef398c45b62a6218308a2f7bcaff0d50d872b
sorbari2016 <152311585+Sorbari2016@users.noreply.github.com>
2026-04-23 16:25:14 +0100

    Update the queries file

--- queries.sql
+++ queries.sql
@@ -20,9 +20,9 @@ CREATE TABLE reading_log (
 );
 
 
--- Create a reviews, note or comments table
-CREATE TABLE reviews (
-    review_id SERIAL PRIMARY KEY, 
+-- Create a summary, note or comments table
+CREATE TABLE summary (
+    summary_id SERIAL PRIMARY KEY, 
     note TEXT, 
     book_read_id NOT NULL REFERENCES reading_log(log_id)
 ); 
@@ -35,7 +35,7 @@ INSERT INTO books (title, author)
 INSERT INTO reading_log (date_read, rating, book_id)
     VALUES('2021-11-17', 5, 1); 
 
-INSERT INTO reviews (note, book_read_id) 
+INSERT INTO summary (note, book_read_id) 
     VALUES(`
         Atomic habit is a personal development book by James Clear that teaches how to cultivate good habits 
         and eliminate bad ones using certain laws, principles and real life examples, applying them bits by bits. 
@@ -48,8 +48,8 @@ INSERT INTO reviews (note, book_read_id)
 SELECT bk.title, 
        bk.author, 
        rl.rating, 
-       rv.note AS summary
+       sm.note AS note
 FROM books AS  bk
 JOIN reading_log AS rl ON bk.id = rl.book_id
-JOIN reviews AS rv ON rl.book_id = rv.book_read_id; 
+JOIN summary AS sm ON rl.book_id = sm.book_read_id; 
 
